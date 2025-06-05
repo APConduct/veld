d362cbad94a3871c68b7796bdeba126a196d905e
Perry <aidanpj18@gmail.com>
2025-06-05 19:50:10 -0400

    refine numeric types

--- core/src/types/numeric.rs
+++ core/src/types/numeric.rs
@@ -10,8 +10,31 @@ pub struct NumericKinds;
 
 impl NumericKinds {
     pub fn register(env: &mut TypeEnvironment) -> Result<()> {
-        // Register the Number kind
+        // Register the Number kind with comprehensive conversion methods
         let mut number_methods = HashMap::new();
+        
+        // Basic type conversion methods
+        number_methods.insert(
+            "as_i8".to_string(),
+            Type::Function {
+                params: vec![Type::KindSelf("Number".to_string())],
+                return_type: Box::new(Type::I8),
+            },
+        );
+        number_methods.insert(
+            "as_i16".to_string(),
+            Type::Function {
+                params: vec![Type::KindSelf("Number".to_string())],
+                return_type: Box::new(Type::I16),
+            },
+        );
+        number_methods.insert(
+            "as_i32".to_string(),
+            Type::Function {
+                params: vec![Type::KindSelf("Number".to_string())],
+                return_type: Box::new(Type::I32),
+            },
+        );
         number_methods.insert(
             "as_i64".to_string(),
             Type::Function {
@@ -20,10 +43,82 @@ impl NumericKinds {
             },
         );
         number_methods.insert(
+            "as_u8".to_string(),
+            Type::Function {
+                params: vec![Type::KindSelf("Number".to_string())],
+                return_type: Box::new(Type::U8),
+            },
+        );
+        number_methods.insert(
+            "as_u16".to_string(),
+            Type::Function {
+                params: vec![Type::KindSelf("Number".to_string())],
+                return_type: Box::new(Type::U16),
+            },
+        );
+        number_methods.insert(
+            "as_u32".to_string(),
+            Type::Function {
+                params: vec![Type::KindSelf("Number".to_string())],
+                return_type: Box::new(Type::U32),
+            },
+        );
+        number_methods.insert(
+            "as_u64".to_string(),
+            Type::Function {
+                params: vec![Type::KindSelf("Number".to_string())],
+                return_type: Box::new(Type::U64),
+            },
+        );
+        number_methods.insert(
+            "as_f32".to_string(),
+            Type::Function {
+                params: vec![Type::KindSelf("Number".to_string())],
+                return_type: Box::new(Type::F32),
+            },
+        );
+        number_methods.insert(
             "as_f64".to_string(),
             Type::Function {
                 params: vec![Type::KindSelf("Number".to_string())],
                 return_type: Box::new(Type::F64),
+            },
+        );
+        
+        // Utility methods
+        number_methods.insert(
+            "is_integer".to_string(),
+            Type::Function {
+                params: vec![Type::KindSelf("Number".to_string())],
+                return_type: Box::new(Type::Bool),
+            },
+        );
+        number_methods.insert(
+            "is_float".to_string(),
+            Type::Function {
+                params: vec![Type::KindSelf("Number".to_string())],
+                return_type: Box::new(Type::Bool),
+            },
+        );
+        number_methods.insert(
+            "is_signed".to_string(),
+            Type::Function {
+                params: vec![Type::KindSelf("Number".to_string())],
+                return_type: Box::new(Type::Bool),
+            },
+        );
+        number_methods.insert(
+            "is_unsigned".to_string(),
+            Type::Function {
+                params: vec![Type::KindSelf("Number".to_string())],
+                return_type: Box::new(Type::Bool),
+            },
+        );
+        number_methods.insert(
+            "type_name".to_string(),
+            Type::Function {
+                params: vec![Type::KindSelf("Number".to_string())],
+                return_type: Box::new(Type::String),
             },
         );
 
@@ -76,14 +171,148 @@ impl NumericKinds {
             vec!["Self".to_string(), "Rhs".to_string(), "Output".to_string()],
         );
 
-        // TODO: Add similar registrations for Sub, Mul, Div, etc.
+        // Subtraction
+        let mut sub_methods = HashMap::new();
+        sub_methods.insert(
+            "sub".to_string(),
+            Type::Function {
+                params: vec![
+                    Type::KindSelf("Self".to_string()),
+                    Type::TypeParam("Rhs".to_string()),
+                ],
+                return_type: Box::new(Type::TypeParam("Output".to_string())),
+            },
+        );
+        env.add_kind(
+            "Sub",
+            sub_methods,
+            HashMap::new(),
+            vec!["Self".to_string(), "Rhs".to_string(), "Output".to_string()],
+        );
+        
+        // Multiplication
+        let mut mul_methods = HashMap::new();
+        mul_methods.insert(
+            "mul".to_string(),
+            Type::Function {
+                params: vec![
+                    Type::KindSelf("Self".to_string()),
+                    Type::TypeParam("Rhs".to_string()),
+                ],
+                return_type: Box::new(Type::TypeParam("Output".to_string())),
+            },
+        );
+        env.add_kind(
+            "Mul",
+            mul_methods,
+            HashMap::new(),
+            vec!["Self".to_string(), "Rhs".to_string(), "Output".to_string()],
+        );
+        
+        // Division
+        let mut div_methods = HashMap::new();
+        div_methods.insert(
+            "div".to_string(),
+            Type::Function {
+                params: vec![
+                    Type::KindSelf("Self".to_string()),
+                    Type::TypeParam("Rhs".to_string()),
+                ],
+                return_type: Box::new(Type::TypeParam("Output".to_string())),
+            },
+        );
+        env.add_kind(
+            "Div",
+            div_methods,
+            HashMap::new(),
+            vec!["Self".to_string(), "Rhs".to_string(), "Output".to_string()],
+        );
+        
+        // Remainder/Modulo
+        let mut rem_methods = HashMap::new();
+        rem_methods.insert(
+            "rem".to_string(),
+            Type::Function {
+                params: vec![
+                    Type::KindSelf("Self".to_string()),
+                    Type::TypeParam("Rhs".to_string()),
+                ],
+                return_type: Box::new(Type::TypeParam("Output".to_string())),
+            },
+        );
+        env.add_kind(
+            "Rem",
+            rem_methods,
+            HashMap::new(),
+            vec!["Self".to_string(), "Rhs".to_string(), "Output".to_string()],
+        );
+        
+        // Exponent
+        let mut pow_methods = HashMap::new();
+        pow_methods.insert(
+            "pow".to_string(),
+            Type::Function {
+                params: vec![
+                    Type::KindSelf("Self".to_string()),
+                    Type::TypeParam("Rhs".to_string()),
+                ],
+                return_type: Box::new(Type::TypeParam("Output".to_string())),
+            },
+        );
+        env.add_kind(
+            "Pow",
+            pow_methods,
+            HashMap::new(),
+            vec!["Self".to_string(), "Rhs".to_string(), "Output".to_string()],
+        );
+        
+        // Negation (unary minus)
+        let mut neg_methods = HashMap::new();
+        neg_methods.insert(
+            "neg".to_string(),
+            Type::Function {
+                params: vec![
+                    Type::KindSelf("Self".to_string()),
+                ],
+                return_type: Box::new(Type::TypeParam("Output".to_string())),
+            },
+        );
+        env.add_kind(
+            "Neg",
+            neg_methods,
+            HashMap::new(),
+            vec!["Self".to_string(), "Output".to_string()],
+        );
 
         Ok(())
     }
 
     fn register_numeric_impl(env: &mut TypeEnvironment, ty: Type) -> Result<()> {
-        // Implement Number kind
+        // Implement Number kind with all conversion methods
         let mut methods = HashMap::new();
+        
+        // Basic type conversion methods
+        methods.insert(
+            "as_i8".to_string(),
+            Type::Function {
+                params: vec![ty.clone()],
+                return_type: Box::new(Type::I8),
+            },
+        );
+        methods.insert(
+            "as_i16".to_string(),
+            Type::Function {
+                params: vec![ty.clone()],
+                return_type: Box::new(Type::I16),
+            },
+        );
+        methods.insert(
+            "as_i32".to_string(),
+            Type::Function {
+                params: vec![ty.clone()],
+                return_type: Box::new(Type::I32),
+            },
+        );
         methods.insert(
             "as_i64".to_string(),
             Type::Function {
@@ -92,10 +321,82 @@ impl NumericKinds {
             },
         );
         methods.insert(
+            "as_u8".to_string(),
+            Type::Function {
+                params: vec![ty.clone()],
+                return_type: Box::new(Type::U8),
+            },
+        );
+        methods.insert(
+            "as_u16".to_string(),
+            Type::Function {
+                params: vec![ty.clone()],
+                return_type: Box::new(Type::U16),
+            },
+        );
+        methods.insert(
+            "as_u32".to_string(),
+            Type::Function {
+                params: vec![ty.clone()],
+                return_type: Box::new(Type::U32),
+            },
+        );
+        methods.insert(
+            "as_u64".to_string(),
+            Type::Function {
+                params: vec![ty.clone()],
+                return_type: Box::new(Type::U64),
+            },
+        );
+        methods.insert(
+            "as_f32".to_string(),
+            Type::Function {
+                params: vec![ty.clone()],
+                return_type: Box::new(Type::F32),
+            },
+        );
+        methods.insert(
             "as_f64".to_string(),
             Type::Function {
                 params: vec![ty.clone()],
                 return_type: Box::new(Type::F64),
+            },
+        );
+        
+        // Utility methods
+        methods.insert(
+            "is_integer".to_string(),
+            Type::Function {
+                params: vec![ty.clone()],
+                return_type: Box::new(Type::Bool),
+            },
+        );
+        methods.insert(
+            "is_float".to_string(),
+            Type::Function {
+                params: vec![ty.clone()],
+                return_type: Box::new(Type::Bool),
+            },
+        );
+        methods.insert(
+            "is_signed".to_string(),
+            Type::Function {
+                params: vec![ty.clone()],
+                return_type: Box::new(Type::Bool),
+            },
+        );
+        methods.insert(
+            "is_unsigned".to_string(),
+            Type::Function {
+                params: vec![ty.clone()],
+                return_type: Box::new(Type::Bool),
+            },
+        );
+        methods.insert(
+            "type_name".to_string(),
+            Type::Function {
+                params: vec![ty.clone()],
+                return_type: Box::new(Type::String),
             },
         );
 
@@ -207,6 +508,53 @@ impl NumericValue {
         match self {
             NumericValue::Integer(iv) => iv.as_i64() == Some(0),
             NumericValue::Float(fv) => fv.as_f64() == 0.0,
+        }
+    }
+    
+    pub fn is_integer(&self) -> bool {
+        matches!(self, NumericValue::Integer(_))
+    }
+    
+    pub fn is_float(&self) -> bool {
+        matches!(self, NumericValue::Float(_))
+    }
+    
+    pub fn is_signed(&self) -> bool {
+        match self {
+            NumericValue::Integer(iv) => matches!(
+                iv,
+                IntegerValue::I8(_) | IntegerValue::I16(_) | IntegerValue::I32(_) | IntegerValue::I64(_)
+            ),
+            NumericValue::Float(_) => true, // Floats are always signed
+        }
+    }
+    
+    pub fn is_unsigned(&self) -> bool {
+        match self {
+            NumericValue::Integer(iv) => matches!(
+                iv,
+                IntegerValue::U8(_) | IntegerValue::U16(_) | IntegerValue::U32(_) | IntegerValue::U64(_)
+            ),
+            NumericValue::Float(_) => false, // Floats are never unsigned
+        }
+    }
+    
+    pub fn type_name(&self) -> String {
+        match self {
+            NumericValue::Integer(iv) => match iv {
+                IntegerValue::I8(_) => "i8".to_string(),
+                IntegerValue::I16(_) => "i16".to_string(),
+                IntegerValue::I32(_) => "i32".to_string(),
+                IntegerValue::I64(_) => "i64".to_string(),
+                IntegerValue::U8(_) => "u8".to_string(),
+                IntegerValue::U16(_) => "u16".to_string(),
+                IntegerValue::U32(_) => "u32".to_string(),
+                IntegerValue::U64(_) => "u64".to_string(),
+            },
+            NumericValue::Float(fv) => match fv {
+                FloatValue::F32(_) => "f32".to_string(),
+                FloatValue::F64(_) => "f64".to_string(),
+            },
         }
     }
 }
