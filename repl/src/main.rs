498055ebbf640f2af0c16c61391badab8d718daa
Perry <aidanpj18@gmail.com>
2025-07-22 07:41:17 -0400

    Merge repl main to allocate stack on all platforms

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -9,58 +9,33 @@ use veld_core::lexer::Lexer;
 use veld_core::parser::Parser;
 
 fn main() -> Result<()> {
-    // On Windows, run in a new thread with a larger stack; otherwise, run normally
-    #[cfg(target_os = "windows")]
-    {
-        let handle = std::thread::Builder::new()
-            .stack_size(16 * 1024 * 1024) // 16 MB stack
-            .spawn(|| {
-                tracing_subscriber::fmt::init();
+    // Always run in a new thread with a larger stack on all platforms
+    let handle = std::thread::Builder::new()
+        .stack_size(16 * 1024 * 1024) // 16 MB stack
+        .spawn(|| {
+            tracing_subscriber::fmt::init();
 
-                // Get command line arguments
-                let args: Vec<String> = env::args().collect();
+            // Get command line arguments
+            let args: Vec<String> = env::args().collect();
 
-                if args.len() == 1 {
-                    let mut repl = Repl::new();
-                    repl.run()
-                        .map_err(|e| veld_core::error::VeldError::RuntimeError(format!("{e}")))?;
-                    Ok(())
-                } else if args.len() == 2 {
-                    // One argument - interpret the file
-                    run_file(&args[1])
-                } else {
-                    println!("Usage: veld [filename]");
-                    println!("       veld           # Run in REPL mode");
-                    Ok(())
-                }
-            })
-            .unwrap();
+            if args.len() == 1 {
+                let mut repl = Repl::new();
+                repl.run()
+                    .map_err(|e| veld_core::error::VeldError::RuntimeError(format!("{e}")))?;
+                Ok(())
+            } else if args.len() == 2 {
+                // One argument - interpret the file
+                run_file(&args[1])
+            } else {
+                println!("Usage: veld [filename]");
+                println!("       veld           # Run in REPL mode");
+                Ok(())
+            }
+        })
+        .unwrap();
 
-        // Propagate any error from the thread
-        handle.join().unwrap()
-    }
-
-    #[cfg(not(target_os = "windows"))]
-    {
-        tracing_subscriber::fmt::init();
-
-        // Get command line arguments
-        let args: Vec<String> = env::args().collect();
-
-        if args.len() == 1 {
-            let mut repl = Repl::new();
-            repl.run()
-                .map_err(|e| veld_core::error::VeldError::RuntimeError(format!("{e}")))?;
-            Ok(())
-        } else if args.len() == 2 {
-            // One argument - interpret the file
-            run_file(&args[1])
-        } else {
-            println!("Usage: veld [filename]");
-            println!("       veld           # Run in REPL mode");
-            Ok(())
-        }
-    }
+    // Propagate any error from the thread
+    handle.join().unwrap()
 }
 
 fn run_file(filename: &str) -> Result<()> {
